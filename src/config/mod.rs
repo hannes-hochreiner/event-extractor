64924b7df5cb2510cffd46da2e76f280d060b0ea
Hannes Hochreiner <hannes@hochreiner.net>
2023-08-02 17:26:34 +0200

    remove unused code

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -34,30 +34,3 @@ impl Config {
         )?)
     }
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn parse_test1() {
-//         let text = r#"
-//             {
-//                 "entries": [
-//                     {"input": "/path/input", "output": "/path/output", "remove_files": true}
-//                 ]
-//             }
-//         "#;
-
-//         assert_eq!(
-//             serde_json::from_str::<Config>(text).unwrap(),
-//             Config {
-//                 entries: vec![Entry {
-//                     input: "/path/input".into(),
-//                     output: "/path/output".into(),
-//                     remove_files: true
-//                 }]
-//             }
-//         )
-//     }
-// }
